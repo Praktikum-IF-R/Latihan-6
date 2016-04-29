24096f5c17d66d7a5237794628e193afee1ca10e
frhn9 <just_fadhyl@hotmail.co.id>
2016-04-29 17:40:53 +0700

    155150207111146_Fadhyl Farhan A

---
+++ Filkom/Fakultas.java
@@ -0,0 +1,22 @@
+package Filkom;
+
+public class Fakultas {
+    private String fakultas, rektor, univ;
+    
+    public Fakultas(String f, String r, String u){
+        this.fakultas = f;
+        this.rektor = r;
+        this.univ = u;
+    }
+    public String getFakultas(){
+        return fakultas;
+    }
+    public String getRektor(){
+        return rektor;
+    }
+    public String getUniv(){
+        return univ;
+    }
+    
+    
+}
